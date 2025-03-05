81f729ff0a9b000c04111bb54ea358298ceac66c
blackdranzer099 <blackdranzer099@gmail.com>
2025-03-05 15:02:28 +0530

    small changes done for docker

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; // Removed unused 'Navigate'
-import "./Pages/variables.css"; // Import global CSS variables
-import "./Pages/base.css"; // Import global base styles
+import "./pages/variables.css"; // Import global CSS variables
+import "./pages/base.css"; // Import global base styles
 import Navbar from "./components/Navbar"; // Import Navbar component
 import Footer from "./components/Footer"; // Import Footer component
 import ParticleBackground from "./components/ParticleBackground"; // Import ParticleBackground component
-import Home from "./Pages/Home"; // Import Home component
-import About from "./Pages/About"; // Import About component
-import Services from "./Pages/Services"; // Import Services component
-import Testimonials from "./Pages/Testimonials"; // Import Testimonials component
-import ContactForm from "./Pages/Contactus"; // Import ContactForm component
-import DemoFormPage from "./Pages/DemoFormPage"; // Import DemoFormPage component
+import Home from "./pages/Home"; // Import Home component
+import About from "./pages/About"; // Import About component
+import Services from "./pages/Services"; // Import Services component
+import Testimonials from "./pages/Testimonials"; // Import Testimonials component
+import ContactForm from "./pages/Contactus"; // Import ContactForm component
+import DemoFormPage from "./pages/DemoFormPage"; // Import DemoFormPage component
 import RegisterForm from './components/RegisterForm'; // Import RegisterForm component
 import Pricing from "./components/Pricing"; // Import Pricing Page
 import "bootstrap/dist/css/bootstrap.min.css";
